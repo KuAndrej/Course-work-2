af415c6d4d7729ef96c1f26c483abbf856f1ddba
AndrejDV <andry-84@mail.ru>
2022-05-25 22:32:18 +0300

    continuation of work from 25.05.22(1)

--- tests/api_test.py
+++ tests/api_test.py
@@ -24,17 +24,4 @@ class TestApi:
         assert response.status_code == 200, "Статус код запроса всех постов неверный"
         assert response.mimetype == "application/json", "Получен не JSON"
 
-    # def test_app_all_posts_type_count_content(self):
-    #     """Проверяем, правильные ли данные получены"""
-    #
-    #     response = app.test_client().get('/api/posts', follow_redirects=True)
-    #     assert type(response.json) == list, "Получен не список"
-    #     assert len(response.json) == 8, "Получено неверное количество элементов в списке"
-    #
-    # def test_app_all_posts_type_check_keys(self):
-    #     """Проверяем, правильные ли данные получены"""
-    #
-    #     keys = {"poster_name", "poster_avatar", "pic", "content", "views_count", "likes_count", "pk"}
-    #     response = app.test_client().get('/api/posts', follow_redirects=True)
-    #     first_keys = set(response.json[0].keys())
-    #     assert keys == first_keys, "Полученные ключи не совпадают"
+
